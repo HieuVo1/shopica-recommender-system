adc40fdff57f1269a2405c8f24009b4771ded093
HieuVo1 <hieuvo044@gmail.com>
2021-05-19 19:18:04 +0700

    initial

--- shopica_recommendation/urls.py
+++ shopica_recommendation/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
-
-from product.models import Product
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('predict/', include('product.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
